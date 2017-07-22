fb3d5d809c156b20d1797f35aa04ad64d2e711bd
Joseph Carrington <joseph.carrington@gmail.com>
2017-07-21 20:21:44 -0500

    index.php

--- index.php
+++ index.php
@@ -147,14 +147,6 @@ function vulcan_video_upload() {
   wp_die($filename);
 }
 
-function var_error_log( $object=null ){
-  ob_start();                    // start buffer capture
-  var_dump( $object );           // dump the values
-  $contents = ob_get_contents(); // put the buffer into a variable
-  ob_end_clean();                // end capture
-  error_log( $contents );        // log contents of the result of var_dump( $object )
-}
-
 add_action('wp_ajax_vulcan_video_clear_and_insert', 'vulcan_video_clear_and_insert');
 function vulcan_video_clear_and_insert() {
   if(!current_user_can('import')) {
